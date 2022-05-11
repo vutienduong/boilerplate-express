3a49bbb8e430e2529e6e3547059cbeb7c3d6acf0
Jin Dong <lucifervtd@gmail.com>
2022-05-11 21:00:51 +0000

    add json

--- myApp.js
+++ myApp.js
@@ -7,6 +7,9 @@ app.use('/public', express.static(__dirname + '/public'))
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
+app.get("/json", (req, res) => {
+  res.json({message: "Hello json"})
+})
 
 
 module.exports = app;
