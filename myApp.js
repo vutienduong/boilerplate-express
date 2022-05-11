a944fc7f8f8b61e4fa25d0be26cfaa4d8480baa3
Jin Dong <lucifervtd@gmail.com>
2022-05-11 20:51:30 +0000

    add style

--- myApp.js
+++ myApp.js
@@ -2,8 +2,10 @@ let express = require("express");
 let app = express();
 
 console.log("Hello World");
+app.use(express.static(__dirname + "/public/style.css") )
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
+
 
 module.exports = app;
