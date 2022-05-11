b5e63798b8640702c5b3e026e74f5bacbde6d949
Jin Dong <lucifervtd@gmail.com>
2022-05-11 20:52:54 +0000

    add

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require("express");
 let app = express();
 
 console.log("Hello World");
-app.use(express.static(__dirname + "/public/style.css") )
+app.use("/", express.static(__dirname + "/public/style.css") )
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
