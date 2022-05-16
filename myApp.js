51b33a6af91bb6f4a4c3817bbb76a8884bcf9216
Jin Dong <lucifervtd@gmail.com>
2022-05-16 14:31:47 +0000

    fix post

--- myApp.js
+++ myApp.js
@@ -32,7 +32,7 @@ app.get('/:word/echo', (req, res) => {
 app.route('/name').get((req, res) => {
   res.json({name: `${req.query.first} ${req.query.last}`})
 }).post((req, res) => {
-  res.json({name: `${req.query.first} ${req.query.last}`})
+  res.json({name: `${req.body.first} ${req.body.last}`})
 })
 
 module.exports = app;
