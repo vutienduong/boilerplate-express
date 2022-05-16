04bad69b639a9233ec6ee6252d17f9f71164fd61
Jin Dong <lucifervtd@gmail.com>
2022-05-16 14:19:20 +0000

    add name endpoint

--- myApp.js
+++ myApp.js
@@ -26,6 +26,9 @@ app.get("/now", (req, res, next) => {
 app.get('/:word/echo', (req, res) => {
   res.json({echo: req.params.word})
 })
+app.get('/name', (req, res) => {
+  res.json({name: `${req.query.first} ${req.query.last}`})
+})
 
 module.exports = app;
 
