c119de5b7b51f4e772ab1afde257dc8e7cd7a1e0
Jin Dong <lucifervtd@gmail.com>
2022-05-16 14:30:25 +0000

    add post

--- myApp.js
+++ myApp.js
@@ -29,7 +29,9 @@ app.get("/now", (req, res, next) => {
 app.get('/:word/echo', (req, res) => {
   res.json({echo: req.params.word})
 })
-app.get('/name', (req, res) => {
+app.route('/name').get((req, res) => {
+  res.json({name: `${req.query.first} ${req.query.last}`})
+}).post((req, res) => {
   res.json({name: `${req.query.first} ${req.query.last}`})
 })
 
